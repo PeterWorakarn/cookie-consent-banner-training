601e46b339a0ecd158f754480f24909d2082bce3
Worakarn Oun <skboardgames@gmail.com>
2022-04-12 00:51:23 +0700

    Update consent-init.js

--- cookie/consent-init.js
+++ cookie/consent-init.js
@@ -25,7 +25,7 @@ cc.run({
 
   gui_options: {
     consent_modal: {
-      layout: 'cloud', // box/cloud/bar
+      layout: 'bar', // box/cloud/bar
       position: 'bottom center', // bottom/middle/top + left/right/center
       transition: 'slide', // zoom/slide
       swap_buttons: false, // enable to invert buttons
